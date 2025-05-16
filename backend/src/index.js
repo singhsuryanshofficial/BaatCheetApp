b0b4d8de598fad3dc852d1cc6c68a1b3a01ccd9e
Suryansh Singh <160277989+singhsuryanshofficial@users.noreply.github.com>
2025-05-16 23:51:34 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,6 +28,8 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
